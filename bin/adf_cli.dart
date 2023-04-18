98d8ebcb72983f179ea341264dfb69c3339777b5
filipemendes18 <filipem0512@gmail.com>
2023-04-18 15:32:06 -0300

    getting started

--- bin/adf_cli.dart
+++ bin/adf_cli.dart
@@ -1,1 +1,31 @@
-void main(List<String> arguments) {}
+import 'package:args/command_runner.dart';
+
+void main(List<String> arguments) {
+  // final argParser = ArgParser();
+  // argParser.addFlag('data', abbr: 'd');
+  // argParser.addOption('name', abbr: 'n');
+  // argParser.addOption('template', abbr: 't');
+  // argParser.parse(arguments);
+
+  CommandRunner('ADF CLI', 'ADF CLI')
+    ..addCommand(ExemploCommand())
+    ..run(arguments);
+}
+
+class ExemploCommand extends Command {
+  @override
+  String get description => 'Command example';
+
+  @override
+  String get name => 'example';
+
+  ExemploCommand() {
+    argParser.addOption('template',
+        abbr: 't', help: 'Project creation template');
+  }
+
+  @override
+  void run() {
+    print('Execute anything');
+  }
+}
